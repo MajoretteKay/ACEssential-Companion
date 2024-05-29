e2e75d3efadff6ba3a109e407b4ae84c33a74210
keolecki <kay@autessa.com>
2024-05-29 16:46:00 -0700

    Init Springboot

---
+++ src/main/java/ACEssential_Companion/AcEssentialCompanionApplication.java
@@ -0,0 +1,13 @@
+package kay.olecki.ACEssential_Companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcEssentialCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcEssentialCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kay/olecki/ACEssential_Companion/AcEssentialCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package kay.olecki.ACEssential_Companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcEssentialCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
